c93de0fe3adad8874aa89ba6256c6ea7b164ad61
alinaattiq04 <alinaattiq04@gmail.com>
2025-12-30 22:30:46 -0500

    Add analytical SQL queries for hitters performance

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -14,3 +14,15 @@ LIMIT 10;
 -- Value concentration: total WAR
 SELECT SUM(WAR) AS total_hitter_war
 FROM hitters_2025;
+
+-- WAR efficiency (WAR per 600 PA)
+SELECT
+  Player,
+  PA,
+  WAR,
+  ROUND(WAR * 600.0 / PA, 2) AS WAR_per_600_PA,
+  "OPS+"
+FROM hitters_2025
+WHERE PA >= 100
+ORDER BY WAR_per_600_PA DESC
+LIMIT 10;
